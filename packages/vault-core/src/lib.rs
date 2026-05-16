cd353155eb7a04201bc415a0542a213a1c0df9ca
Adrian Kucharczyk <16486151+akei9@users.noreply.github.com>
2026-05-16 02:06:00 +0200

    TASK-008: export vault-core API

--- packages/vault-core/src/lib.rs
+++ packages/vault-core/src/lib.rs
@@ -4,3 +4,7 @@ pub mod error;
 pub mod generator;
 pub mod types;
 pub mod vault;
+
+pub use error::VaultError;
+pub use generator::{GeneratorConfig, GeneratorMode};
+pub use types::{KdfConfig, VaultEntry, VaultKey, VaultMeta};
